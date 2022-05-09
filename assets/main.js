effcf3941aa729ff2b593edb3bfb29d202824969
Muzamil Khan <muzamil-JavaScriptDev@outlook.com>
2022-05-09 23:23:47 +0500

    updated

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,18 @@
 //initialize slider
 new Glide(".glide", {
     type: "carousel",
-    gap: 15,
+    gap: 10,
     autoplay: 4000,
     hoverpause: true,
     keyboard: true,
     perView: 1,
     peek:    { 
       before: 0, after: 50 
+    },
+    breakpoints: {
+         1600: {
+             perView: 2
+           },
     },
   
   }).mount();
