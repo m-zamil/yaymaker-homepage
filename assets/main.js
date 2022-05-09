9e004c036f519e1c11b9dfe5e1e9435cc2ffe6b4
Muzamil Khan <muzamil-JavaScriptDev@outlook.com>
2022-05-10 01:07:26 +0500

    inperson section completed

--- assets/main.js
+++ assets/main.js
@@ -28,3 +28,35 @@ new Glide(".glide", {
     },
   
   }).mount();
+
+  //initialize slider
+new Glide(".glide1", {
+  type: "carousel",
+  gap: 10,
+  autoplay: 4000,
+  hoverpause: true,
+  keyboard: true,
+  perView: 1,
+  peek:    { 
+    before: 0, after: 50 
+  },
+  direction:"rtl",
+  breakpoints: {
+       1600: {
+           perView: 2,
+           peek:    { 
+            before: 0, after: 70,
+          },
+          gap:20,
+
+         },
+         960:{
+           perView:1,
+           peek:    { 
+            before: 0, after: 50 
+          },
+          gap:15
+         }
+  },
+
+}).mount();
