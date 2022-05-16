ffc3394642f9828b84770154a8641783ec5e87a1
Muzamil Khan <muzamil-JavaScriptDev@outlook.com>
2022-05-17 01:08:24 +0500

    top experience section cards...

--- assets/main.js
+++ assets/main.js
@@ -63,3 +63,36 @@ new Glide(".glide1", {
     },
   },
 }).mount();
+
+//initialize slider
+new Glide(".glideTopexp", {
+  type: "carousel",
+  gap: 10,
+  autoplay: 4000,
+  hoverpause: true,
+  keyboard: true,
+  perView: 1,
+  peek: {
+    before: 0,
+    after: 50,
+  },
+  breakpoints: {
+    1600: {
+      perView: 2,
+      peek: {
+        before: 0,
+        after: 70,
+      },
+      gap: 20,
+    },
+
+    960: {
+      perView: 1.3,
+      peek: {
+        before: 0,
+        after: 0,
+      },
+      gap: 0,
+    },
+  },
+}).mount();
