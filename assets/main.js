1bb6a5d11362f49717e13452bc9e106eebf7c95a
Muzamil Khan <muzamil-JavaScriptDev@outlook.com>
2022-06-29 20:46:46 +0500

    glide sliders fixed

--- assets/main.js
+++ assets/main.js
@@ -26,26 +26,17 @@ window.onload = function () {
   //initialize slider
   new Glide(".glide1", {
     type: "carousel",
-    gap: 10,
+    gap: 20,
     autoplay: 4000,
     hoverpause: true,
     keyboard: true,
-    perView: 1,
+    perView: 2,
     peek: {
       before: 0,
-      after: 50,
+      after: 70,
     },
     direction: "rtl",
     breakpoints: {
-      19200: {
-        perView: 2,
-        peek: {
-          before: 0,
-          after: 70,
-        },
-        gap: 20,
-      },
-
       960: {
         perView: 1,
         peek: {
@@ -60,25 +51,16 @@ window.onload = function () {
   //initialize slider
   new Glide(".glideTopexp", {
     type: "carousel",
-    gap: 10,
+    gap: 0,
     autoplay: 4000,
     hoverpause: true,
     keyboard: true,
-    perView: 1,
+    perView: 3,
     peek: {
       before: 0,
-      after: 50,
+      after: 0,
     },
     breakpoints: {
-      19200: {
-        perView: 3,
-        peek: {
-          before: 0,
-          after: 0,
-        },
-        gap: 0,
-      },
-
       960: {
         perView: 1.3,
         peek: {
